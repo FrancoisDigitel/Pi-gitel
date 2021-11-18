e30f2b2bb665e242385d8cf94f1ca36f816ff0cc
Francois Durieu Digitel <durieu@digitelstudios.lu>
2021-11-18 22:47:40 +0100

    added first chromium script

---
+++ chromium-script/full-screen-page/full-screen-page-script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#before starting this script be sure to have vnc and ssh enabled for future managent.
+#Preferences > raspberry pi configuration > interface >(enable ssh and vnc)
+
+#wifi can also be used for this
+
+#update pi to newest version
+
+sudo apt update -y && sudo apt full-upgrade -y
+
+#create autostart directory and file
+sudo mkdir -p .config/lxsession/LXDE-pi
+sudo nano .config/lxsession/LXDE-pi/autostart
+
+#add start parameter
+echo "
+@lxpanel --profile LXDE-pi
+@pcmanfm --desktop --profile LXDE-pi
+#@xscreensaver -no-splash
+point-rpi
+@chromium-browser --start-fullscreen --start-maximized https://Google.com/
+
+" >> .config/lxsession/LXDE-pi/autostart
+
+#change hdmi output 1 to left 
+
+DISPLAY=:0 xrandr --output HDMI-1 --rotate left
